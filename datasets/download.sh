16f22d739c47c26bd0488ac874bf712dca3fbc2a
Harsha Kokel <harsha.kokel@ibm.com>
2025-07-15 16:21:18 -0500

    adding wikijoin

--- datasets/download.sh
+++ datasets/download.sh
@@ -8,4 +8,24 @@ cd autojoin
 mkdir -p datalake
 sh ./create_datalake.sh
 rm -rf ./autojoin/original
-echo "AutoJoin Setup"
+echo "AutoJoin Setup Done"
+
+
+
+# Wiki Join
+
+echo "Downloading WikiJoin"
+rm -rf ./wikijoin/wiki-join-search
+rm -rf ./wikijoin/datalake
+rm ./wikijoin/gt.jsonl
+wget -O ./wikijoin/original.tar.bz2 https://zenodo.org/records/10042019/files/wiki-join-search.tar.bz2
+echo "Setting up Wiki Join"
+cd wikijoin
+tar -xvzf gt.jsonl.tar.gz
+mkdir ./datalake
+tar -xjf ./original.tar.bz2
+mv wiki-join-search/tables-with-headers/ datalake
+rm -r wiki-join-search
+rm -r ./original.tar.bz2
+echo "WikiJoin Setup Done"
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+   name='ContextAwareJoin',
+   packages=['josie','topjoin','embedjoin','topjoin_es_knn','topjoin_standalone','LSH_ensemble','deepjoin','deepjoinFT'],
+   package_dir={'':'src'}
+)
